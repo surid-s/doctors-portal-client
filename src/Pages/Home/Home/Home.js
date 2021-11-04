df5427f3eb9848394f85676e51de15f5d3eea8e3
surid-s <maruf15-9549@diu.edu.bd>
2021-11-04 20:27:54 +0600

    home and services added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../../Shared/Navigation/Navigation'
+import Services from '../Services/Services';
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Services></Services>
+        </div>
+    );
+};
+
+export default Home;
